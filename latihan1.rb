da6468f4c9135d7eb83ec87d9aba9b401b1b124a
dafanf <dafanurulfauziansyah97@gmail.com>
2022-11-18 10:57:19 +0700

    update latihan 1 adding dynamic year checker

--- latihan1.rb
+++ latihan1.rb
@@ -1,16 +1,15 @@
 # Super class Vehicle
+require 'Date'
 class Vehicle
-
+    attr_accessor :year
     # initializing year field
-    def initialize(y)
-        
-        @year = y
+    def initialize(year)
+        self.year = year
     end
     
     # show year to screen
-    def printStats
-
-        puts @year
+    def print_stats()
+        puts "Year: #{self.year}"
     end
 
 end
@@ -22,28 +21,27 @@ module OldtimerCheck
     def isOldtimer?(year)
 
         # .to_i for converting year(string) to integer
-        year.to_i < 1992
+        year.to_i < (Date.today.year-30)
     end
 end
 
 # sub class Car
 # extends vehicle class
 class Car < Vehicle
-
+    attr_accessor :model, :brand
     # including OldtimerCheck module to Car class
     # now we can call isOldtimer? to our Car's object
     include OldtimerCheck
 
     # initializing
     def initialize(model, brand, year)
-        
-        @model, @brand = model, brand
-
+        self.model = model
+        self.brand = brand
         # use super for calling superclass(Vehicle) constructor
         super(year)
     end
 
-    def printStats
+    def print_stats()
         puts ("Specification :\nModel : #{@model.chomp} \nBrand : #{@brand.chomp} \nYear : #{@year}")
     end
 end
@@ -60,7 +58,7 @@ brand = gets
 
 # create new Car object
 newCar = Car.new(model, brand, year)
-newCar.printStats
+newCar.print_stats
 
 # Call isOldtimer from Module
 if newCar.isOldtimer?(year) == true
